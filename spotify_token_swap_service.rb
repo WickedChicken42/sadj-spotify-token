7387a69698ffe5292fe96c05182c102f0de6c014
Bilawal Hameed <bil@spotify.com>
2018-02-04 20:31:01 +0100

    fixup

--- spotify_token_swap_service.rb
+++ spotify_token_swap_service.rb
@@ -7,24 +7,6 @@ require "base64"
 require "encrypted_strings"
 require "httparty"
 
-class String
-  def encrypt(key)
-    cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').encrypt
-    cipher.key = Digest::SHA1.hexdigest key
-    s = cipher.update(self) + cipher.final
-
-    s.unpack('H*')[0].upcase
-  end
-
-  def decrypt(key)
-    cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').decrypt
-    cipher.key = Digest::SHA1.hexdigest key
-    s = [self].pack("H*").unpack("C*").pack("c*")
-
-    cipher.update(s) + cipher.final
-  end
-end
-
 module SpotifyTokenSwapService
 
   # SpotifyTokenSwapService::ConfigHelper
