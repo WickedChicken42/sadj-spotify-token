640db11ff41abb2d2bd7eeaa1d0861aa13ce7dc3
Bilawal Hameed <bil@spotify.com>
2018-02-04 02:25:24 +0100

    added config class

--- spotify_token_swap_service.rb
+++ spotify_token_swap_service.rb
@@ -7,6 +7,26 @@ require "encrypted_strings"
 require "dotenv/load"
 
 module SpotifyTokenSwapService
+  set :root, File.dirname(__FILE__)
+
+  # SpotifyTokenSwapService::Config
+  #
+  # This deals with configuration, loaded through .env
+  #
+  class Config < Struct.new(:client_id, :client_secret,
+                            :client_callback_url, :encryption_secret)
+    def initialize
+      @client_id = ENV["SPOTIFY_CLIENT_ID"]
+      @client_secret = ENV["SPOTIFY_CLIENT_SECRET"]
+      @client_callback_url = ENV["SPOTIFY_CLIENT_CALLBACK_URL"]
+      @encryption_secret = ENV["ENCRYPTION_SECRET"]
+    end
+  end
+
+  # SpotifyTokenSwapService::App
+  #
+  # The code needed to make it go all Sinatra, beautiful.
+  #
   class App < Sinatra::Base
     post "/api/swap_for_access_token" do
       "Hi world"
