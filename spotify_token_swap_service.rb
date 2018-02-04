ef28cc0513213b6b40a83ba9b2ee28e7018dcef5
Bilawal Hameed <bil@spotify.com>
2018-02-04 14:07:01 +0100

    starting on error handling

--- spotify_token_swap_service.rb
+++ spotify_token_swap_service.rb
@@ -1,13 +1,17 @@
 require "sinatra"
-require "base64"
-require "json"
-require "active_support/all"
-require "encrypted_strings"
+require "sinatra/json"
+require "sinatra/reloader" if File.exists?(".env")
 require "dotenv/load" if File.exists?(".env")
+require "active_support/all"
+require "base64"
+require "encrypted_strings"
 require "net/http"
 require "net/https"
 
 module SpotifyTokenSwapService
+
+  # SpotifyTokenSwapService::ConfigHelper
+  # SpotifyTokenSwapService::ConfigError
   # SpotifyTokenSwapService::Config
   #
   # This deals with configuration, loaded through .env
@@ -18,13 +22,21 @@ module SpotifyTokenSwapService
     end
   end
 
+  class ConfigError < StandardError
+    def self.empty
+      new("client credentials are empty")
+    end
+  end
+
   class Config < Struct.new(:client_id, :client_secret,
                             :client_callback_url, :encryption_secret)
     def initialize
-      self.client_id = ENV["SPOTIFY_CLIENT_ID"]
-      self.client_secret = ENV["SPOTIFY_CLIENT_SECRET"]
-      self.client_callback_url = ENV["SPOTIFY_CLIENT_CALLBACK_URL"]
-      self.encryption_secret = ENV["ENCRYPTION_SECRET"]
+      # self.client_id = ENV["SPOTIFY_CLIENT_ID"]
+      # self.client_secret = ENV["SPOTIFY_CLIENT_SECRET"]
+      # self.client_callback_url = ENV["SPOTIFY_CLIENT_CALLBACK_URL"]
+      # self.encryption_secret = ENV["ENCRYPTION_SECRET"]
+
+      validate_client_credentials
     end
 
     def has_client_credentials?
@@ -35,6 +47,12 @@ module SpotifyTokenSwapService
 
     def has_encryption_secret?
       encryption_secret.present?
+    end
+
+    private
+
+    def validate_client_credentials
+      raise ConfigError.empty unless has_client_credentials?
     end
   end
 
@@ -47,8 +65,16 @@ module SpotifyTokenSwapService
 
     helpers ConfigHelper
 
+    before do
+      content_type :json
+      headers 'Access-Control-Allow-Origin' => '*',
+              'Access-Control-Allow-Methods' => %w(OPTIONS GET POST)
+    end
+
     get "/" do
       config.has_encryption_secret?.inspect
+    rescue StandardError => e
+      json error: "Hi world"
     end
 
     post "/api/swap_for_access_token" do
